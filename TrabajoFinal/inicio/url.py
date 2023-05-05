5658d9149b08df7bc73a655a31cff96c2cce0148
marcalsur <112291318+marcalsur@users.noreply.github.com>
2023-05-05 18:56:55 -0300

    Update url.py

--- TrabajoFinal/inicio/url.py
+++ TrabajoFinal/inicio/url.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.index, name = 'index'),
     path('', views.menu, name = 'menu'),
     path('', views.nosotros, name = 'nosotros'),
-    path('', views.contacto, name = 'contacto'),
+    path('pedido/', views.pedido, name="pedido"),
 ]
